d8eab59d1b28a62ab87a7b5306e2c04bbf6a4551
Owais Khan <owaiskhandawar310@gmail.com>
2020-06-11 06:35:27 +0500

    change Message.js h1 text

--- src/Message.js
+++ src/Message.js
@@ -3,7 +3,7 @@ import React from 'react'
 function Message(props){
     return(
         <div>
-        <h1>Counter value is: {props.counter}</h1>
+        <h1>Counter State: {props.counter}</h1>
         </div>
     )
 } 
